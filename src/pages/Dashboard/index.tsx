c313a8fb61661298624b1f92090bd722549af6df
Thomas Henrique Ribeiro <thomashenrique2@gmail.com>
2021-07-23 20:52:42 -0300

    Commit inicial

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import api from '../../services/api';
+
+const Dashboard: React.FC = () => {
+  return (
+    <>
+      <h1 style={{ color: 'black' }}>Ola Pessoal</h1>
+    </>
+  );
+};
+
+export default Dashboard;
